447762d62e2a539c1f16e81beb78c74cfec6f2c7
meganedoty <meganedoty@gmail.com>
2015-09-23 22:20:35 +0000

    added proposal files, started landing page

--- prelaunch/landing.php
+++ prelaunch/landing.php
@@ -11,5 +11,46 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   </head>
   <body>
+      <div class="jumbotron">
+          <div class="container text-center">
+              <h1>Épanouir</h1>
+              <h2>Headspace in going abroad.</h2>
+          </div>
+      </div>
+      
+        <nav class="navbar navbar-default navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand">Épanouir</a>
+                </div>
+                
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                      <li class="active"><a href="#">Dashboard</a></li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Information <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#">Pre-Departure</a></li>
+                          <li><a href="#">Abroad</a></li>
+                          <li><a href="#">Returning</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="#">About</a></li>
+                    </ul>
+                    
+                    <form class="navbar-form navbar-right">
+                      <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                      </div>
+                      <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    
+                    
+                </div><!-- collapse-->
+            </div><!-- container-fluid-->
+        </nav>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </body>
 </html>
